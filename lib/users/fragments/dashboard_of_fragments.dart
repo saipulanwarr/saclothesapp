e243116f829044e890ea7fb3dd1eb421758cfefb
saipulanwarr <saipul0994@gmail.com>
2023-12-03 20:51:34 +0700

    create fragments screen and specify fragments nav button properties

--- lib/users/fragments/dashboard_of_fragments.dart
+++ lib/users/fragments/dashboard_of_fragments.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:saclothesapp/users/fragments/favorites_fragment_screen.dart';
+import 'package:saclothesapp/users/fragments/home_fragment_screen.dart';
+import 'package:saclothesapp/users/fragments/order_fragment_screen.dart';
+import 'package:saclothesapp/users/fragments/profile_fragment_screen.dart';
 import 'package:saclothesapp/users/userPreferences/current_user.dart';
 
 class DashboardOfFragments extends StatelessWidget {
   CurrentUser _rememberCurrentUser = Get.put(CurrentUser());
+
+  List<Widget> _fragmentScreens = [
+    HomeFragmentScreen(),
+    FavoritesFragmentScreen(),
+    OrderFragmentScreen(),
+    ProfileFragmentScreen(),
+  ];
+
+  List _navigationButtonsProperties = [
+    {
+      "active_icon": Icons.home,
+      "non_active_icon": Icons.home_outlined,
+      "label": "Home",
+    },
+    {
+      "active_icon": Icons.favorite,
+      "non_active_icon": Icons.favorite_border,
+      "label": "Favorite"
+    },
+    {
+      "active_icon": FontAwesomeIcons.boxOpen,
+      "non_active_icon": FontAwesomeIcons.box,
+      "label": "Orders"
+    },
+    {
+      "active_icon": Icons.person,
+      "non_active_icon": Icons.person_outline,
+      "label": "Profile"
+    },
+  ]
 
   @override
   Widget build(BuildContext context) {
