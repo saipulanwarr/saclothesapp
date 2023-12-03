72dd7f7ec7b3dfdb47563e13e5f8f2769665ff38
saipulanwarr <saipul0994@gmail.com>
2023-12-03 22:08:15 +0700

    implement profile fragment screen ui

--- lib/users/fragments/profile_fragment_screen.dart
+++ lib/users/fragments/profile_fragment_screen.dart
@@ -3,12 +3,83 @@ import 'package:flutter/material.dart';
 class ProfileFragmentScreen extends StatelessWidget {
   const ProfileFragmentScreen({super.key});
 
+  Widget userInfoItemProfile(IconData iconData, String userData) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey,
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8,
+      ),
+      child: Row(
+        children: [
+          Icon(
+            iconData,
+            size: 30,
+            color: Colors.black,
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Text(
+            userData,
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Profile Fragment Screen"),
-      ),
+    return ListView(
+      padding: EdgeInsets.all(32),
+      children: [
+        Center(
+          child: Image.asset(
+            "images/man.png",
+            width: 240,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        userInfoItemProfile(Icons.person, "user name"),
+        const SizedBox(
+          height: 20,
+        ),
+        userInfoItemProfile(Icons.email, "user@gmail.com"),
+        const SizedBox(
+          height: 20,
+        ),
+        Center(
+          child: Material(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(8),
+            child: InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(32),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 12,
+                ),
+                child: Text(
+                  "Sign Out",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
