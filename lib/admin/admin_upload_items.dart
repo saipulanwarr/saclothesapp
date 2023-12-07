f2ffeafa4fe479c57ac609656eafe80897d0dd9a
saipulanwarr <saipul0994@gmail.com>
2023-12-07 12:12:36 +0700

    implement item upload form

--- lib/admin/admin_upload_items.dart
+++ lib/admin/admin_upload_items.dart
@@ -15,6 +15,16 @@ class _AdminUploadItemsState extends State<AdminUploadItems> {
   final ImagePicker _picker = ImagePicker();
   XFile? pickedImageFile;
 
+  var formKey = GlobalKey<FormState>();
+  var nameController = TextEditingController();
+  var ratingController = TextEditingController();
+  var tagsController = TextEditingController();
+  var pricesController = TextEditingController();
+  var sizesController = TextEditingController();
+  var colorsController = TextEditingController();
+  var descriptionController = TextEditingController();
+  var imageLink = "";
+
   captureImageWithPhoneCamera() async {
     pickedImageFile = await _picker.pickImage(source: ImageSource.camera);
 
@@ -104,6 +114,383 @@ class _AdminUploadItemsState extends State<AdminUploadItems> {
                   ),
                 ),
                 fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white24,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(60),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 8,
+                    color: Colors.black26,
+                    offset: Offset(0, -3),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30, 30, 30, 8),
+                child: Column(
+                  children: [
+                    Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: nameController,
+                            validator: (val) =>
+                                val == "" ? "Please write item name" : null,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.title,
+                                color: Colors.black,
+                              ),
+                              hintText: "item namae....",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 14,
+                                vertical: 6,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            controller: ratingController,
+                            validator: (val) =>
+                                val == "" ? "Please give item rating" : null,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.rate_review,
+                                color: Colors.black,
+                              ),
+                              hintText: "item rating....",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 14,
+                                vertical: 6,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            controller: tagsController,
+                            validator: (val) =>
+                                val == "" ? "Please write item tags" : null,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.tag,
+                                color: Colors.black,
+                              ),
+                              hintText: "item tags....",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 14,
+                                vertical: 6,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            controller: pricesController,
+                            validator: (val) =>
+                                val == "" ? "Please write item price" : null,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.price_change_outlined,
+                                color: Colors.black,
+                              ),
+                              hintText: "item price....",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 14,
+                                vertical: 6,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            controller: sizesController,
+                            validator: (val) =>
+                                val == "" ? "Please write item sizes" : null,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.picture_in_picture,
+                                color: Colors.black,
+                              ),
+                              hintText: "item sizes....",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 14,
+                                vertical: 6,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            controller: colorsController,
+                            validator: (val) =>
+                                val == "" ? "Please write item colors" : null,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.color_lens,
+                                color: Colors.black,
+                              ),
+                              hintText: "item color....",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 14,
+                                vertical: 6,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            controller: descriptionController,
+                            validator: (val) => val == ""
+                                ? "Please write item description"
+                                : null,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.description,
+                                color: Colors.black,
+                              ),
+                              hintText: "item description....",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 14,
+                                vertical: 6,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          Material(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(30),
+                            child: InkWell(
+                              onTap: () {
+                                if (formKey.currentState!.validate()) {}
+                              },
+                              borderRadius: BorderRadius.circular(30),
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 28,
+                                ),
+                                child: Text(
+                                  "Upload Now",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                  ],
+                ),
               ),
             ),
           )
