3fef1201f37cfb767437d2670e89dea05ae303dd
saipulanwarr <saipul0994@gmail.com>
2023-12-07 10:38:51 +0700

    implement item uploads default screen

--- lib/admin/admin_upload_items.dart
+++ lib/admin/admin_upload_items.dart
@@ -8,12 +8,76 @@ class AdminUploadItems extends StatefulWidget {
 }
 
 class _AdminUploadItemsState extends State<AdminUploadItems> {
-  @override
-  Widget build(BuildContext context) {
+  Widget defaultScreen() {
     return Scaffold(
-      body: Center(
-        child: Text("Welcome Admin"),
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.black54,
+                Colors.deepPurple,
+              ],
+            ),
+          ),
+        ),
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Welcome Admin",
+          style: TextStyle(
+            color: Colors.white54,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.black54,
+              Colors.deepPurple,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.add_photo_alternate,
+                color: Colors.white54,
+                size: 200,
+              ),
+              Material(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(30),
+                child: InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.circular(30),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 28,
+                    ),
+                    child: Text(
+                      "Add New Item",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return defaultScreen();
   }
 }
